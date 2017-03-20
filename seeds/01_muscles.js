0064838114a940d507005c7c9def6ce5334ebb9f
Paola Claros <paolita.claros@gmail.com>
2017-03-19 20:16:02 -0700

    add migrations files and seeds files

---
+++ seeds/01_muscles.js
@@ -0,0 +1,90 @@
+exports.seed = knex => knex('muscles').del()
+  .then(() => knex('muscles').insert([
+    {
+      id: 1,
+      name: 'Abdominals',
+      size: ''
+    },
+    {
+      id: 2,
+      name: 'Lats',
+      size: ''
+    },
+    {
+      id: 3,
+      name: 'Abductors',
+      size: ''
+
+    },
+    {
+      id: 4,
+      name: 'Lower Back',
+      size: ''
+    },
+    {
+      id: 5,
+      name: 'Adductors',
+      size: ''
+    },
+    {
+      id: 6,
+      name: 'Middle Back',
+      size: ''
+    },
+    {
+      id: 7,
+      name: 'Biceps',
+      size: ''
+    },
+    {
+      id: 8,
+      name: 'Neck',
+      size: ''
+    },
+    {
+      id: 9,
+      name: 'Calves',
+      size: ''
+    },
+    {
+      id: 10,
+      name: 'Quadriceps',
+      size: ''
+    },
+    {
+      id: 11,
+      name: 'Chest',
+      size: 'big'
+    },
+    {
+      id: 12,
+      name: 'Shoulders',
+      size: 'small'
+    },
+    {
+      id: 13,
+      name: 'Forearms',
+      size: ''
+    },
+    {
+      id: 14,
+      name: 'Traps',
+      size: ''
+    },
+    {
+      id: 15,
+      name: 'Glutes',
+      size: ''
+    },
+    {
+      id: 16,
+      name: 'Triceps ',
+      size: ''
+    },
+    {
+      id: 17,
+      name: 'Hamstrings',
+      size: ''
+    }
+  ])
+  );
