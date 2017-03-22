bd67d6bb2a9ddeeee86dd969fb1ae58cb981c041
Michael Martinez <martinez1212@gmail.com>
2017-03-22 13:12:04 -0700

    passing muscle tests; muscles route is working

--- api/controllers/muscles.js
+++ api/controllers/muscles.js
@@ -1,42 +1,17 @@
 const Muscles = require('../../models/muscles');
 
-module.exports.getMuscleById = function(args, res, next) {
-  /**
-   * Returns specific exercise type based on ID
-   *
-   * id Long id of muscle type to get
-   * returns muscles
-   **/
-  var examples = {};
-  examples['application/json'] = {
-  "size" : "aeiou",
-  "name" : "aeiou",
-  "id" : 123456789
-};
-  if (Object.keys(examples).length > 0) {
-    res.setHeader('Content-Type', 'application/json');
-    res.end(JSON.stringify(examples[Object.keys(examples)[0]] || {}, null, 2));
-  } else {
-    res.end();
-  }
+module.exports.getMuscleById = function(req, res, next) {
+  Muscles.where('id', req.swagger.params.id.value).fetch()
+      .then((specificMuscle) => {
+          res.setHeader('Content-Type', 'application/json');
+          res.end(JSON.stringify(specificMuscle));
+      });
 };
 
-module.exports.getAllMuscles = function(args, res, next) {
-  /**
-   * returns all muscle types
-   *
-   * returns List
-   **/
-  var examples = {};
-  examples['application/json'] = [ {
-  "size" : "aeiou",
-  "name" : "aeiou",
-  "id" : 123456789
-} ];
-  if (Object.keys(examples).length > 0) {
-    res.setHeader('Content-Type', 'application/json');
-    res.end(JSON.stringify(examples[Object.keys(examples)[0]] || {}, null, 2));
-  } else {
-    res.end();
-  }
+module.exports.getAllMuscles = function(req, res, next) {
+  Muscles.fetchAll()
+      .then((musclesList) => {
+          res.setHeader('Content-Type', 'application/json');
+          res.end(JSON.stringify(musclesList));
+      });
 };
