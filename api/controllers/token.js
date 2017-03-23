fbbb37e691998d57129575acf2fd716a05cf93df
Paola Claros <paolita.claros@gmail.com>
2017-03-23 11:11:19 -0700

    updating token.js

--- api/controllers/token.js
+++ api/controllers/token.js
@@ -1,24 +1,26 @@
 const bcrypt = require('bcrypt-as-promised');
 const Users = require('../../models/users');
 const cookieParser = require('cookie-parser');
+const dotenv = require('dotenv').config();
+
 
 const JWT = require('jsonwebtoken');
 
-const cert = process.env.JWT;
+const cert = process.env.JWT_KEY;
 
-let createUserToken = function (req, res, next) {
+module.exports.createUserToken = function (req, res, next) {
   console.log("im here");
-  return Users.where('email', '=', req.body.email).fetch()
+  return Users.where('email', '=', req.swagger.params.credentials.value.email).fetch()
 .then((userInfo) => {
   let user = JSON.parse(JSON.stringify(userInfo));
-  return bcrypt.compare(req.body.password, user.hashed_password);
+  return bcrypt.compare( req.swagger.params.credentials.value.password, user.hashed_password);
 })
 .catch((err) => {
   res.header('Content-Type', 'text/plain');
   res.status(400).send('Invalid email or password');
   next(err);
 })
-.then(() => Users.where('email', '=', req.body.email))
+.then(() => Users.where('email', '=',  req.swagger.params.credentials.value.email))
 .fetch()
 .then((userResult) => {
   let user = JSON.parse(JSON.stringify(userResult));
@@ -28,7 +30,13 @@ let createUserToken = function (req, res, next) {
   const token = JWT.sign(claims, cert, {
     expiresIn: '2 hours'
   });
-  res.cookie('token', token, { path: '/', httpOnly: true });
-  res.send('successful operation');
+  user.token = token;
+  delete user.first_name;
+  delete user.last_name;
+  delete user.hashed_password;
+  delete user.updated_at;
+  delete user.created_at;
+  // res.cookie('token', token, { path: '/', httpOnly: true });
+  // res.send('successful operation');
 });
 };
